5ddccbdf4ed9f90eca0586fd50e21eeafc76f0ff
Martin Sarsale <martin@sumavisos.com>
2012-05-28 18:27:01 -0300

    Added street code lookup

--- lib/geokit-geocoders-usig_caba.rb
+++ lib/geokit-geocoders-usig_caba.rb
@@ -7,22 +7,35 @@ module Geokit
     class Usigcaba < Geocoder
       require "cgi"
       require "proj4"
-      # Replae name 'External' (below) with the name of your custom geocoder class
-      # and use :external to specify this geocoder in your list of geocoders.
+
+      # Geocode using USIG - GCBA geocoder
+      # options:
+      #   :lookup_code => try to find the street code from USIG DB before geocoding
       def self.do_geocode(address, options = {})
         res = GeoLoc.new
         res.provider = 'USIG - GCBA'
         numero = address.scan(/([0-9]+)/).flatten.first.to_i
-        calle = address.gsub(/([0-9]+)/,"").strip
-        url="http://ws.usig.buenosaires.gob.ar/geocoder/2.2/geocoding/?cod_calle=#{CGI.escape(calle)}&altura=#{CGI.escape(numero.to_s)}"
+        calle_name = calle = address.gsub(/([0-9]+)/,"").strip
+        if options[:lookup_code]
+          calle, calle_name, other_names, ranges = find_street_code(calle, numero) 
+          logger.debug("Calle: #{calle_name} => (#{numero}) => cod: #{calle}")
+        end
+        url="http://ws.usig.buenosaires.gob.ar/geocoder/2.2/geocoding/?cod_calle=#{CGI.escape(calle.to_s)}&altura=#{CGI.escape(numero.to_s)}"
         resp = do_get(url)
-        return {} if resp.code != '200'
-        data = resp.body[1 .. -2]
+        if resp.code != '200'
+          logger.debug("Geocoder response #{url}: #{resp.code}")
+          res.success = false
+          return res
+        end
+
+        data = resp.body[1 .. -2] # remove wrapping "(" and ")" from response (JSON-P)
+
         if data == '"ErrorCalleInexistente"'
           logger.debug("Calle o número inexistente: '#{calle}' '#{numero}': #{url} #{data}")
           res.success = false
           return res 
         end
+
         begin
           pos = MultiJson.decode(data)
         rescue
@@ -30,6 +43,7 @@ module Geokit
           res.success = false
           return res 
         end
+
         proj = Proj4::Projection.new("+proj=tmerc +lat_0=-34.629269 +lon_0=-58.4633 +k=0.9999980000000001 +x_0=100000 +y_0=100000 +ellps=intl +units=m +no_defs ")
         begin
           pos_latlon = proj.inverseDeg(Proj4::Point.new(pos['x'].to_f, pos['y'].to_f))
@@ -38,10 +52,11 @@ module Geokit
           res.success = false
           return res 
         end
+
         res.success = true
         res.lat = pos_latlon.lat 
         res.lng = pos_latlon.lon 
-        res.street_name = calle
+        res.street_name = calle_name
         res.street_number = numero
         res.street_address = "#{calle} #{numero}"
         res.city = "Ciudad de Buenos Aires"
@@ -49,8 +64,48 @@ module Geokit
         res
       end
 
-      def self.parse_http_resp(body) # :nodoc:
-        # Helper method to parse http response. See geokit/geocoders.rb.
+      # Returns an array of strings from USIG, tries to memoize the data
+      def self.streets
+        @streets ||= begin
+          req = do_get("http://servicios.usig.buenosaires.gov.ar/callejero?")
+          data = req.body
+          logger.debug("got HTTP #{req.code}, #{data.length} bytes from http://servicios.usig.buenosaires.gov.ar/callejero") 
+          streets = MultiJson.decode(data)
+          streets.each{|s|
+            s[1].force_encoding("ISO-8859-1")
+            s[2].force_encoding("ISO-8859-1")
+          }
+          streets
+        end
+      end
+
+      # Find street code from USIG DB
+      def self.find_street_code(name,street_number=false)
+        words = name.upcase.split.compact.sort.uniq
+        candidates = streets.find_all{|c| # finds candidates based on the name 
+          match_words(c[2],words)
+        }
+        logger.debug("Street candidates for #{name} #{street_number}: #{candidates}")
+
+        if street_number # filters out candidates using the street number ranges
+          candidates = candidates.find_all{|street|
+            street[3].find{|range_from, range_to|
+              range_to >= street_number && range_to >= street_number 
+            }
+          }
+        end
+        logger.debug("Candidate for #{name}: #{candidates}")
+        candidates.first
+      end
+      private
+
+      # Checks all words against a string. Returns false if any of them is not found
+      def self.match_words(string,words)
+        words_re = words.map{|w| Regexp.new("(\\s|^)#{w}(\\s|$)")}
+        words_re.each{|w| 
+          logger.debug("#{string}.index(#{w})")
+          return false if not string.index(w)
+        }
       end
     end
   end
--- test/test_usig_caba.rb
+++ test/test_usig_caba.rb
@@ -1,6 +1,10 @@
 require "geokit-geocoders-usig_caba"
 require "test/unit"
+require "logger"
 class TestUsigCaba < Test::Unit::TestCase
+  def setup
+    Geokit::Geocoders.logger = Logger.new(STDERR)
+  end
   def test_valid_address
     res = Geokit::Geocoders::Usigcaba.geocode("Chenaut, Indalesio, Gral. Av. 1724")
     assert(res.success, "Error quering geocoder")
@@ -9,6 +13,28 @@ class TestUsigCaba < Test::Unit::TestCase
     assert_equal("Chenaut, Indalesio, Gral. Av.", res.street_name, "Bad Street Name")
     assert_equal(1724, res.street_number, "Bad Street Number")
   end
+  def test_lookup_address_code
+    res = Geokit::Geocoders::Usigcaba.geocode("Chenaut 1724", lookup_code: true)
+    assert(res.success, "Error quering geocoder")
+    assert_equal(-34.56998029273584, res.lat, "Bad lat")
+    assert_equal(-58.43087213419635, res.lng, "Bad lon")
+    assert_equal("CHENAUT, INDALESIO, GRAL. AV.", res.street_name, "Bad Street Name")
+    assert_equal(1724, res.street_number, "Bad Street Number")
+  end
+  def test_lookup_address_code_choose_by_range
+    # filters out the street INCA because the number is outside the range
+    res = Geokit::Geocoders::Usigcaba.geocode("Inca 4050", lookup_code: true)
+    assert(res.success, "Error quering geocoder")
+    assert_equal("PUENTE DEL INCA", res.street_name, "Bad Street Name")
+    assert_equal(4050, res.street_number, "Bad Street Number")
+  end
+  def test_lookup_address_code_filter_by_range
+    res = Geokit::Geocoders::Usigcaba.geocode("Inca 3857", lookup_code: true)
+    assert(res.success, "Error quering geocoder")
+    assert_equal("INCA", res.street_name, "Bad Street Name")
+    assert_equal(3857, res.street_number, "Bad Street Number")
+  end
+
   def test_invalid_address
     res = Geokit::Geocoders::Usigcaba.geocode("Foobar 1724")
     refute(res.success, "Error quering geocoder")
